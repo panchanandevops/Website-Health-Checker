c35bd988c0d919db1de31a630096c16db3111dc5
Panchanan Panigrahi <panchanandevops@gmail.com>
2024-01-25 21:47:10 +0530

    comments are added

--- check.go
+++ check.go
@@ -6,11 +6,20 @@ import (
 	"time"
 )
 
+// Check function checks the reachability of a given destination and port
 func Check(destination string, port string) string {
+
+	// Construct the full address using the destination and port
 	address := destination + ":" + port
+
+	// Set the timeout duration for the connection attempt
 	timeout := time.Duration(5 * time.Second)
+
+	// Attempt to establish a TCP connection with a timeout
 	conn, err := net.DialTimeout("tcp", address, timeout)
+
 	var status string
+
 	if err != nil {
 		status = fmt.Sprintf("[DOWN] %v is unreachable,\n Error: %v", destination, err)
 	} else {
@@ -18,5 +27,6 @@ func Check(destination string, port string) string {
 			conn.LocalAddr(),
 			conn.RemoteAddr())
 	}
+
 	return status
 }
--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// main.go
 package main
 
 import (
@@ -9,16 +10,19 @@ import (
 )
 
 func main() {
+	// Define the command-line application
 	app := &cli.App{
 		Name:  "Healthchecker",
 		Usage: "A tiny tool that checks the given domain is down.",
 		Flags: []cli.Flag{
+			// Define the domain flag
 			&cli.StringFlag{
 				Name:     "domain",
 				Aliases:  []string{"d"},
 				Usage:    "Domain name to check.",
 				Required: true,
 			},
+			// Define the port flag
 			&cli.StringFlag{
 				Name:     "port",
 				Aliases:  []string{"p"},
@@ -27,16 +31,19 @@ func main() {
 			},
 		},
 		Action: func(c *cli.Context) error {
+			// Extract values from flags or use defaults
 			port := c.String("port")
 			if c.String("port") == "" {
 				port = "80"
 			}
+			// Call the Check function and print the result
 			status := Check(c.String("domain"), port)
 			fmt.Println(status)
 			return nil
 		},
 	}
 
+	// Run the CLI application
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
